c5fe62df0044c559c93be799678f354f0232b39a
Steve Grunwell <stevegrunwell@gmail.com>
2011-01-01 23:35:13 -0500

    Added support for .barker-close, script now (mostly) passes JSLint

--- barker.js
+++ barker.js
@@ -7,72 +7,23 @@
 	To do:
 	Allow users to set options when initiating Barker
 		- Anchor
-		- Icon directory
 	Limit the number of consecutive notices
 	
 */
-
-/* Inititate Barker */
-function barker(opts){
-	/* Append #barker-notices, the wrapping <ul />, to the DOM */
-	$('body').append('<ul id="barker-notices"></div>');
-	
-	this.settings = new Object;
-	this.count = 0;
-	this.timers = new Array;
-	this.options = barkerSetOpts;
-	this.notice = barkerNotice;
-	this.show = showBarkerNotice;
-	this.pause = pauseBarkerNotice;
-	this.resume = resumeBarkerNotice;
-	this.hide = hideBarkerNotice;
-	this.hover = hoverBarkerNotice;
-	return true;
-}
 
 /* Set options for Barker */
 function barkerSetOpts(opts){
 	if( typeof(opts) == 'object' ){
 		barker.settings.iconpath = ( typeof(opts.path) == 'string' ? opts.path : 'icons/' );
-		barker.settings.limit = ( typeof(opts.limit) != 'undefined' && parseInt(opts.limit) > 0 ? parseInt(opts.limit) : 6 );
+		barker.settings.limit = ( typeof(opts.limit) != 'undefined' && parseInt(opts.limit, 10) > 0 ? parseInt(opts.limit, 10) : 6 );
+		barker.settings.timeout = ( typeof(opts.frequency) != 'undefined' && parseInt(opts.frequency, 10) > 0 ? parseInt(opts.frequency, 10) : 20000 );
 	}
-}
-
-/* Display a notice */
-function barkerNotice(msg, ico, duration){
-	if( msg != '' ){
-		ico = typeof(ico) != 'undefined' ? ico : 'notice.png';
-		duration = typeof(duration) != 'undefined' ? parseInt(duration) : 5000;
-		
-		/* Assemble the barker notice */
-		barker.count++;
-		id = barker.count;
-		barkerNotice = '<li id="barker-notice-' + id + '" class="barker-notice' + ( duration <= 0 ? ' persist' : '' ) + '" style="display:none;">' + '<img src="' + barker.settings.iconpath + ico + '" alt="" />' + msg + '</li>';
-		
-		/* Show the barker notice */
-		$('#barker-notices').prepend(barkerNotice);
-		$('#barker-notice-' + id).bind('mouseover mouseenter', function(){
-			var inst = this.id.substr(14);
-			pauseBarkerNotice(inst);
-			hoverBarkerNotice(inst);
-		});
-		$('#barker-notice-' + id).bind('mouseout mouseleave', function(){
-			if( duration > 0 ){
-				resumeBarkerNotice(this.id.substr(14));
-			}
-		});
-		$('#barker-notice-' + id).bind('click', function(){
-			hideBarkerNotice(this.id.substr(14));
-		});
-		barker.show(id, duration);
-	}
-	return true;
 }
 
 /* Show a barker notice */
 function showBarkerNotice(id, duration){
 	$('#barker-notice-' + id).fadeIn(400, function(){
-		if( parseInt(duration) > 0 ){
+		if( parseInt(duration, 10) > 0 ){
 			barker.timers[id] = window.setTimeout('hideBarkerNotice(' + id + ')', duration);
 		}
 	});
@@ -107,6 +58,56 @@ function hoverBarkerNotice(id){
 	$('#barker-notice-' + id).addClass('barker-hover').bind('mouseout mouseleave', function(){
 		$(this).removeClass('barker-hover');
 	});
+	return true;
+}
+
+/* Display a notice */
+function barkerNotice(msg, ico, duration){
+	if( barker.enabled && msg !== '' ){
+		ico = typeof(ico) != 'undefined' ? ico : 'notice.png';
+		duration = typeof(duration) != 'undefined' ? parseInt(duration, 10) : 5000;
+		
+		/* Assemble the barker notice */
+		barker.count++;
+		id = barker.count;
+		notice = '<li id="barker-notice-' + id + '" class="barker-notice' + ( duration <= 0 ? ' persist' : '' ) + '" style="display:none;">' + '<span class="barker-close">Close</span><img src="' + barker.settings.iconpath + ico + '" alt="" />' + msg + '</li>';
+		
+		/* Show the barker notice */
+		$('#barker-notices').prepend(notice);
+		$('#barker-notice-' + id).bind('mouseover mouseenter', function(){
+			var inst = this.id.substr(14);
+			pauseBarkerNotice(inst);
+			hoverBarkerNotice(inst);
+		});
+		$('#barker-notice-' + id).bind('mouseout mouseleave', function(){
+			if( duration > 0 ){
+				resumeBarkerNotice(this.id.substr(14));
+			}
+		});
+		$('#barker-notice-' + id).bind('click', function(){
+			hideBarkerNotice(this.id.substr(14));
+		});
+		barker.show(id, duration);
+	}
+	return true;
+}
+
+/* Inititate Barker */
+function barker(opts){
+	/* Append #barker-notices, the wrapping <ul />, to the DOM */
+	$('body').append('<ul id="barker-notices"></div>');
+	
+	this.settings = {};
+	this.count = 0;
+	this.timers = [];
+	this.options = barkerSetOpts;
+	this.notice = barkerNotice;
+	this.show = showBarkerNotice;
+	this.pause = pauseBarkerNotice;
+	this.resume = resumeBarkerNotice;
+	this.hide = hideBarkerNotice;
+	this.hover = hoverBarkerNotice;
+	return true;
 }
 
 /* Assign the global variable */
